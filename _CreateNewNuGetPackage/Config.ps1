f67fa135a761a1874fe800b7e02362c1ba9fed6b
hpsanampudi <hpsanampudi@gmail.com>
2017-10-20 11:08:04 +0100

    Signed assembly with strong name.

--- _CreateNewNuGetPackage/Config.ps1
+++ _CreateNewNuGetPackage/Config.ps1
@@ -32,7 +32,7 @@ $platform = "AnyCPU"
 #	e.g. $packProperties = "TargetFrameworkVersion=v3.5;Optimize=true"
 # Do not specify the "Configuration" or "Platform" here; use the $configuration and $platform variables above.
 # MsBuild Properties that can be specified: http://msdn.microsoft.com/en-us/library/vstudio/bb629394.aspx
-$packProperties = "TargetFrameworkVersion=v4.6;Optimize=true"
+$packProperties = "TargetFrameworkVersion=v4.6.1;Optimize=true"
 
 # Specify any NuGet Pack options to pass to nuget.exe.
 #	e.g. $packOptions = "-Symbols"
