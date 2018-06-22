a0bf9b465fe5d2550f66ebfb34b7bf6cba189b22
hpsanampudi <hpsanampudi@gmail.com>
2018-06-22 17:03:46 +0100

    T4 templates build action to None

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number - MonthDate
 //      Revision - HoursMinutes
 //
-[assembly: AssemblyVersion("3.0.0.2")]
-[assembly: AssemblyFileVersion("3.0.0.2")]
+[assembly: AssemblyVersion("3.0.0.3")]
+[assembly: AssemblyFileVersion("3.0.0.3")]
 
